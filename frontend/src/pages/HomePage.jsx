88eeef4e94b397f7c8755c3c0ead1383d45476c5
R D <rusirud49@gmail.com>
2026-01-28 19:11:03 +0530

    create files

---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "../components/Navbar.jsx";
+
+const HomePage = () => {
+    return (
+        <div>
+            <div>Home Page</div>
+        </div>
+    );
+};
+
+export default HomePage;
